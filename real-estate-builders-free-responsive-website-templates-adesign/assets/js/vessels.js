b59719a495ba048c4a446897e7c7a716b3100baa
Henry Gruen <hmgruen@gmail.com>
2022-05-02 11:23:34 -0500

    both modals work

--- real-estate-builders-free-responsive-website-templates-adesign/assets/js/vessels.js
+++ real-estate-builders-free-responsive-website-templates-adesign/assets/js/vessels.js
@@ -6,23 +6,12 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("ModalLink");
 
-// Get the modal
-var modal2 = document.getElementById("myModal2");
-
-// Get the button that opens the modal
-var btn2 = document.getElementById("ModalLink2");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
-}
-
-// When the user clicks on the button, open the modal
-btn2.onclick = function () {
-    modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -37,8 +26,27 @@ window.onclick = function (event) {
     }
 }
 
+
+
+
+// MODAL 2
+
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("ModalLink2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function () {
+    modal2.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+span2.onclick = function () {
     modal2.style.display = "none";
 }
 
@@ -48,6 +56,55 @@ window.onclick = function (event) {
         modal2.style.display = "none";
     }
 }
+
+
+
+
+
+
+
+
+// MODAL 3
+
+// Get the modal
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("ModalLink3");
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close3")[0];
+
+// When the user clicks on the button, open the modal
+btn3.onclick = function () {
+    modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function () {
+    modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
