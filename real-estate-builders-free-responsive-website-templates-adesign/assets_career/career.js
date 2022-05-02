3c72d9e5bbfd72d20061c5b07cc3b74f7e2054a4
Anthony Lai <anthonylai89@yahoo.com>
2022-05-01 22:05:34 -0500

    getting close to fixing the modals

--- real-estate-builders-free-responsive-website-templates-adesign/assets_career/career.js
+++ real-estate-builders-free-responsive-website-templates-adesign/assets_career/career.js
@@ -227,40 +227,32 @@ window.onclick = function(event) {
 }
 
 // Get the modal
-var modal = document.getElementsById("myModal");
+var modal2 = document.getElementById("myModal2");
 
 // Get the button that opens the modal
-var btn = document.getElementsById("myBtn");
+var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
+var span2 = document.getElementsByClassName("close")[1];
 
 // When the user clicks the button, open the modal 
-btn[0].onclick = function() {
-  modal[0].style.display = "block";
-}
-btn[1].onclick = function() {
-    modal[1].style.display = "block";
+btn2.onclick = function() {
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span[0].onclick = function() {
-    modal[0].style.display = "none";
-}
-
-span[1].onclick = function() {
-    mode[1].style.display = "none";
+span2.onclick = function() {
+  modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal[0]) {
-    modal[0].style.display = "none";
-  }
-  if (event.target == modal[1]) {
-      modal[1].style.display = "none";
+  if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
+
+
 
 
 db.collection('jobs_available').get().then((data) => {
