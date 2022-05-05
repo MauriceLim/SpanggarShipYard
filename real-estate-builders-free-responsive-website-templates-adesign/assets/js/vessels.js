211d1ab6b07475022eb15034a9cb7d696416e480
MauriceLim <mauricelim2601@gmail.com>
2022-05-05 13:15:26 -0500

    minor mistake

--- real-estate-builders-free-responsive-website-templates-adesign/assets/js/vessels.js
+++ real-estate-builders-free-responsive-website-templates-adesign/assets/js/vessels.js
@@ -346,7 +346,7 @@ function load_vessel_admin(filter) {
         <div class="card">
           <header class="card-header">
             <p class="card-header-title">
-               RM${vessel.data().Price}
+               RM ${vessel.data().Price}
             </p>
             <div class="box">
             ${vessel.data().Status}
@@ -491,7 +491,7 @@ function load_vessel_client(filter) {
         <div class="card">
           <header class="card-header">
             <p class="card-header-title">
-              RM${vessel.data().Price}
+              RM ${vessel.data().Price}
             </p>
             <div class="box">
             ${vessel.data().Status}
@@ -531,7 +531,7 @@ function load_vessel_client(filter) {
           <div class="card">
             <header class="card-header">
             <p class="card-header-title">
-            ${vessel.data().Vessel}
+            RM ${vessel.data().Price}
           </p>
           <div class="box">
           ${vessel.data().Status}
